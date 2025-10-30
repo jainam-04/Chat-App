1feea6f794c32098a0af24e36a859cf927477382
Jainam Rupani <jainamrupani04@gmail.com>
2025-10-30 18:12:30 +0530

    Created message mapping to sending messages

--- src/main/java/com/app/controller/ChatController.java
+++ src/main/java/com/app/controller/ChatController.java
@@ -1,8 +1,15 @@
 package com.app.controller;
 
+import com.app.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
-
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/messages")
+    public ChatMessage sendMessage(ChatMessage message){
+        return message;
+    }
 }
